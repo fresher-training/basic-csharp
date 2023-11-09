3845247590470f1f275b751be4a8ebb1c3c667c8
vosonha89 <vosonha89@gmail.com>
2023-11-09 20:47:09 +0700

    init project for 500 a e

---
+++ basic-csharp/basic-csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Create 4 objects: user, product, cart, order - database schema 
+ * Create SQL Adappter: AppDbAdapter - insert, update, delete, select
+ * Create cart service: add product to user's cart
+ * Create order service: create user order - add products from user's cart to order; delete products in cart
+ */
+
+using System.Text;
+
+string a = "";
+a += "1312312";
+
+StringBuilder b = new StringBuilder();
+b.Append(1312312);
+b.ToString();
+
+List<string> list = new List<string>() { "1", "2", "3", "4", "5" };
+for (int i = 0; i < list.Count; i++)
+{
+    if (list[i] == "3")
+    {
+        Console.WriteLine(list[i]);
+    }
+}
+
+Console.WriteLine(list.Where(a=>a == "3").FirstOrDefault());
+
+Console.WriteLine(a);
+Console.WriteLine(b.ToString());
+Console.ReadLine();
---
+++ basic-csharp/basic-csharp/SQLAdapter/UserSQLAdapter.cs
@@ -0,0 +1,39 @@
+﻿namespace basic_csharp.SQLAdapter
+{
+    public class UserSQLAdapter : ISQLAdapter
+    {
+        public string ConnectionString { get; set; }
+        public string TableName { get; set; }
+
+        public UserSQLAdapter(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+            this.TableName = "USER";
+        }
+
+        public int Delete<T>(Guid id) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(Guid id) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetData<T>() where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert<T>(T item) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update<T>(T item) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
